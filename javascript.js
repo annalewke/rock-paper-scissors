b9bedf598a5666a02dcb1621b2e232ee22c6d406
Anna Maria <123685928+annalewke@users.noreply.github.com>
2023-12-01 15:11:23 +0100

    Create basic UI to play 5 rounds

--- javascript.js
+++ javascript.js
@@ -3,8 +3,11 @@ let computerScore = 0;
 let roundWinner = "";
 let endGame = "";
 
-let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-const computerSelection = getComputerChoice();
+const buttons = Array.from(document.querySelectorAll("button"));
+
+buttons.forEach(function(button){
+    button.addEventListener("click", function(){playRound(button.value)})
+})
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3);
@@ -17,47 +20,48 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+let resultDiv = document.querySelector("#result");
+let playerWins = document.querySelector("#playerwins");
+let comWins = document.querySelector("#comwins");
+playerWins.textContent = "Player wins: 0";
+comWins.textContent = "Computer wins: 0";
+
+function playRound(playerSelection) {
+   let computerSelection = getComputerChoice(); 
     if (playerSelection === computerSelection) {
-        return roundWinner = "tie"
+        resultDiv.innerHTML += "<p>tie</p>";
+        return roundWinner = "tie";
     } else if (
     playerSelection == "rock" && computerSelection == "scissors" ||
     playerSelection == "paper" && computerSelection == "rock" ||
     playerSelection == "scissors" && computerSelection == "paper") {
-        playerScore += 1
-        return roundWinner = "player!";
+        playerScore += 1;
+        resultDiv.innerHTML += "<p>player</p>";
+        playerWins.textContent = `Player wins: ${playerScore}`;
+        if (playerScore === 5) {
+            alert("player wins!")
+            resultDiv.innerHTML = "";
+            playerScore = 0;
+            computerScore = 0;
+            playerWins.textContent = "Player wins: 0";
+            comWins.textContent = "Computer wins: 0";
+        }
+        return roundWinner = "player";
     } else if (
     playerSelection == "rock" && computerSelection == "paper" ||
     playerSelection == "paper" && computerSelection == "scissors" ||
     playerSelection == "scissors" && computerSelection == "rock") {
-        computerScore += 1
-        return roundWinner = "computer!";
+        computerScore += 1;
+        resultDiv.innerHTML += "<p>computer</p>";
+        comWins.textContent = `Computer wins: ${computerScore}`;
+        if (computerScore === 5) {
+            alert("computer wins!")
+            resultDiv.innerHTML = "";
+            playerScore = 0;
+            computerScore = 0;
+            playerWins.textContent = "Player wins: 0";
+            comWins.textContent = "Computer wins: 0";
+        }
+        return roundWinner = "computer";
     }
 }
-
-function game() {
-    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    playRound(playerSelection, computerSelection)
-    console.log(roundWinner)
-    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    playRound(playerSelection, computerSelection)
-    console.log(roundWinner)
-    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    playRound(playerSelection, computerSelection)
-    console.log(roundWinner)
-    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    playRound(playerSelection, computerSelection)
-    console.log(roundWinner)
-    playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-    playRound(playerSelection, computerSelection)
-    console.log(roundWinner)
-    if (playerScore > computerScore) {
-        return "You won with a score of: " + playerScore;
-    } else if (playerScore < computerScore) {
-        return "You lost, the computer won with a score of: " + computerScore; 
-    } else {
-        return "no one won, the scor sits at " + playerScore + " and " + computerScore
-    }
-}
-
-console.log(game())
